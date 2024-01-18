da5cf86f301c7c81b3b4ab65ccea49d02ada241b
Yelnur <elnur.kazakhstan@gmail.com>
2024-01-18 15:10:31 +0600

    Update app.js

--- assignment2/app.js
+++ assignment2/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 app.listen(port, () => {
