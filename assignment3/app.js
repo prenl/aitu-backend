b38e6af0131b556a04e7c62bb34719b443c308fd
Yelnur Abdrakhmanov <79540438+prenl@users.noreply.github.com>
2024-02-12 03:45:03 +0600

    feat(assignment3): deploy settings

--- assignment3/app.js
+++ assignment3/app.js
@@ -266,8 +266,8 @@ app.get("/logout", async (req, res) => {
 });
 
 // Listening
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server is running on ${port}`);
 });
 
 
